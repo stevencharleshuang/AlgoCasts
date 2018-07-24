c7666ecfaa2de277681a1f6e5a86b24a45d78273
Steven Huang <steven.charles.huang@gmail.com>
2018-07-24 18:18:20 -0400

    completed maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let max = 0
+  let foundMaxChar = ''
+  let charsMap = {};
+
+  for (let char of str) {
+    charsMap[char] == null
+      ? charsMap[char] = 1
+      : charsMap[char] = charsMap[char] + 1;
+  }
+
+  for (let char in charsMap) {
+    if (charsMap[char] > max) {
+      max = charsMap[char]
+      foundMaxChar = char
+    }
+  }
+
+  return foundMaxChar
+}
+
+maxChar('fuunny');
 
 module.exports = maxChar;
