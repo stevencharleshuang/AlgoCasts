9892f53a901f10b3ddd6f34e881a8186a7a8990d
Steven Huang <steven.charles.huang@gmail.com>
2018-07-24 22:05:40 -0400

    completed anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,46 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+/* My Solution */
+function anagrams(stringA, stringB) {
+  let charsMapA = {};
+  let charsMapB = {};
+
+  stringA = stringA.replace(/[^\w]/g, '').toLowerCase();
+
+  stringB = stringB.replace(/[^\w]/g, '').toLowerCase();
+
+  for (let char of stringA) {
+    charsMapA[char] == null
+      ? charsMapA[char] = 1
+      : charsMapA[char] = charsMapA[char] + 1
+  }
+
+  for (let char of stringB) {
+    charsMapB[char] == null
+      ? charsMapB[char] = 1
+      : charsMapB[char] = charsMapB[char] + 1
+  }
+
+  isAnagram = () => {
+    if (Object.keys(charsMapA).length !== Object.keys(charsMapB).length) {
+      return false;
+
+    } else {
+      for (let char in charsMapA) {
+        if (charsMapA[char] === charsMapB[char]) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+
+  return isAnagram()
+}
+
+/* Solution 1 */
+// function anagrams(stringA, stringB) {}
 
 module.exports = anagrams;
