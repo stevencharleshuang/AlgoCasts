f6596a203bca43b9605db48b8f5df9e4c5fc8825
Steven Huang <steven.charles.huang@gmail.com>
2018-07-24 22:20:04 -0400

    completed anagrams alt solutions

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -9,45 +9,76 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 /* My Solution */
+// function anagrams(stringA, stringB) {
+//   let charsMapA = {};
+//   let charsMapB = {};
+
+//   stringA = stringA.replace(/[^\w]/g, '').toLowerCase();
+
+//   stringB = stringB.replace(/[^\w]/g, '').toLowerCase();
+
+//   for (let char of stringA) {
+//     charsMapA[char] == null
+//       ? charsMapA[char] = 1
+//       : charsMapA[char] = charsMapA[char] + 1
+//   }
+
+//   for (let char of stringB) {
+//     charsMapB[char] == null
+//       ? charsMapB[char] = 1
+//       : charsMapB[char] = charsMapB[char] + 1
+//   }
+
+//   isAnagram = () => {
+//     if (Object.keys(charsMapA).length !== Object.keys(charsMapB).length) {
+//       return false;
+
+//     } else {
+//       for (let char in charsMapA) {
+//         if (charsMapA[char] === charsMapB[char]) {
+//           return true;
+//         } else {
+//           return false;
+//         }
+//       }
+//     }
+//   }
+
+//   return isAnagram();
+// }
+
+/* Alternate Solution 1 */
+// function anagrams(stringA, stringB) {
+//   const aCharMap = buildCharMap(stringA);
+//   const bCharMap = buildCharMap(stringB);
+
+//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//     return false;
+//   }
+//   for(let char in aCharMap) {
+//     if (aCharMap[char] !== bCharMap[char]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// function buildCharMap(str) {
+//   const charMap = {};
+
+//   for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+//   return charMap;
+// }
+
+/* Alternate Solution 2 */
 function anagrams(stringA, stringB) {
-  let charsMapA = {};
-  let charsMapB = {};
-
-  stringA = stringA.replace(/[^\w]/g, '').toLowerCase();
-
-  stringB = stringB.replace(/[^\w]/g, '').toLowerCase();
-
-  for (let char of stringA) {
-    charsMapA[char] == null
-      ? charsMapA[char] = 1
-      : charsMapA[char] = charsMapA[char] + 1
-  }
-
-  for (let char of stringB) {
-    charsMapB[char] == null
-      ? charsMapB[char] = 1
-      : charsMapB[char] = charsMapB[char] + 1
-  }
-
-  isAnagram = () => {
-    if (Object.keys(charsMapA).length !== Object.keys(charsMapB).length) {
-      return false;
-
-    } else {
-      for (let char in charsMapA) {
-        if (charsMapA[char] === charsMapB[char]) {
-          return true;
-        } else {
-          return false;
-        }
-      }
-    }
-  }
-
-  return isAnagram()
+  return cleanString(stringA) === cleanString(stringB);
 }
 
-/* Solution 1 */
-// function anagrams(stringA, stringB) {}
+function cleanString(str) {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
 
 module.exports = anagrams;
