2d8333675bb3a02731ea5813b9d504c95dd4519d
stevencharleshuang <steven.charles.huang@gmail.com>
2018-09-05 18:28:51 -0400

    completed steps algo

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,36 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// function steps(n) {
+//   let hash = '#';
+//   let space = ' ';
+  
+//   for(let row = 0; row < n; row += 1) {
+//     let tree = '';
+//     for (let column = 0; column < n; column += 1) {
+//       if (column <= row) {
+//         tree += hash;
+//       } else {
+//         tree += space;
+//       }
+//     }
+//     console.log(tree);
+//   }
+// }
+
+function steps(n, row = 0, step = '') {
+  if (n === row) {
+    return;
+  } 
+
+  if (n === step.length) {
+    console.log(step);
+    return steps(n, row + 1);
+  }
+
+  const add = (step.length <= row) ? '#' : ' '
+
+  steps(n, row, step + add);
+}
 
 module.exports = steps;
